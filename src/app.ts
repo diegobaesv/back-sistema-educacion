3fa118a1a3756d59ccc8f650b10e69a3b386fbe6
Diego Baes <diegobaesv@gmail.com>
2024-07-05 21:00:56 -0500

    instalado morgan

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express, { Application } from 'express';
+import logger from 'morgan';
 
 const app: Application = express();
 
 app.use(express.json());
+app.use(logger('dev'));
 
 export default app;
