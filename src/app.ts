03f00b1cdb714387a8c9e5b5ad736e59ed3346e4
Diego Baes <diegobaesv@gmail.com>
2024-07-05 21:35:53 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import logger from 'morgan';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(logger('dev'));
+
+app.get('/saludar', (req: Request, res:Response) => {
+    res.send('Hola desde Nodejs');
+});
 
 export default app;
